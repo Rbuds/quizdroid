7d74b4691fdc29aefdc35bc5d95fe2acfa039aa0
Ryan Budnik <ryan.budnik@outlook.com>
2015-05-18 23:39:15 -0700

    progress

---
+++ app/src/main/java/com/example/ryan/quizdroid/QuizApp.java
@@ -0,0 +1,11 @@
+package com.example.ryan.quizdroid;
+
+import android.app.Application;
+
+/**
+ * Created by Ryan on 5/14/2015.
+ */
+public class QuizApp extends Application implements TopicRepository {
+
+
+}
---
+++ app/src/main/java/com/example/ryan/quizdroid/TopicRepository.java
@@ -0,0 +1,13 @@
+package com.example.ryan.quizdroid;
+
+/**
+ * Created by Ryan on 5/14/2015.
+ */
+
+import java.util.List;
+
+public interface TopicRepository {
+
+    Topic getTopic();
+    void setTopic(String value);
+}
